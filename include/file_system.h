4e430b74a5918c5d1256479a51ef11840e2204d5
Jack <ys3xia@uwaterloo.ca>
2026-06-08 22:28:49 -0400

    Add a comment

--- include/file_system.h
+++ include/file_system.h
@@ -15,17 +15,8 @@ struct FileEntry {
 };
 
 // Idea:
-//  -> Each FileOpState corresponds to a single "fd"'s life cycle.
-//  -> File open operation against the same file path, usnig the same callback, are considered distinct because 
-//     underneath, a new fd is acquired from the OS.
-//  -> The life cycle is linear like this:
-//      -> fs.open(). We persist the provided open JS callback, to be invoked when libuv retrieves an fd successfully
-//          -> When this is done, this JS function can be free'd. Update state
-//      -> fs.read(fd), we start reading from the fd, from the start, providing buffer length. Again, persist the read callback to FileOp
-//          -> When libuv read callback is invoked, we free this read_callback JS function. Update state
-//              -> TODO: This is assuming we are processing stuff in one shot. Does not support file stream yet.
-//      -> fs.close(fd):
-//          -> When libuv "close" callback is invoked, os has reclaimed the fd, and we can cleanup this data structure
+//  -> FileOpState is fairly "static". It just tracks the fd across fs.* calls
+//  -> It does not track the callbacks. Those are created/free'd on an per-call basis. See fs.cc
 struct FileOpState {
     int fd = -1;
     char *buf = nullptr;
